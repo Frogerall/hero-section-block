6fe2f19c3183c0ddc1a3f49d321ce6100ef55266
Frogerall <saliticoadrian@gmail.com>
2023-11-21 22:02:37 +0800

    added dropdown menu

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ navClose.addEventListener("click", () => {
  navPanel.classList.remove("nav-open");
  navOverlay.classList.remove("nav-unhide");
 });
+
+let dropone = document.querySelector(".dropdown-content-1");
+let droptwo = document.querySelector(".dropdown-content-2");
+let navRotate = document.querySelector(".nav-arrow-1");
+let navRotateTwo = document.querySelector(".nav-arrow-2");
+
+document.querySelector(".dropdown-1").addEventListener("click", (event) => {
+ event.preventDefault();
+ dropone.classList.toggle("nav-unhide");
+ navRotate.classList.toggle("nav-rotate");
+});
+document.querySelector(".dropdown-2").addEventListener("click", (event) => {
+ event.preventDefault();
+ droptwo.classList.toggle("nav-unhide");
+ navRotateTwo.classList.toggle("nav-rotate");
+});
