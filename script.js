09d8a2e43f7a54fcdc69976fd4c72585206bc210
Frogerall <saliticoadrian@gmail.com>
2023-11-21 18:39:19 +0800

    added overlay on the mobile nav bar

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let navMenu = document.querySelector(".nav-menu");
 let navPanel = document.querySelector(".nav-panel");
 let navClose = document.querySelector(".nav-close");
+let navOverlay = document.querySelector(".nav-overlay");
 
 navMenu.addEventListener("click", () => {
  navPanel.classList.toggle("nav-open");
+ navOverlay.classList.toggle("nav-unhide");
 });
+
 navClose.addEventListener("click", () => {
  navPanel.classList.remove("nav-open");
+ navOverlay.classList.remove("nav-unhide");
 });
