3bfe0cd47db5e2e87cda07a92db80d6cfd7731dd
Frogerall <saliticoadrian@gmail.com>
2023-11-22 14:42:48 +0800

    finish fixing adjustments

--- script.js
+++ script.js
@@ -17,14 +17,45 @@ let dropone = document.querySelector(".dropdown-content-1");
 let droptwo = document.querySelector(".dropdown-content-2");
 let navRotate = document.querySelector(".nav-arrow-1");
 let navRotateTwo = document.querySelector(".nav-arrow-2");
+let navAnchor = document.querySelectorAll(".nav-anchor");
 
 document.querySelector(".dropdown-1").addEventListener("click", (event) => {
  event.preventDefault();
- dropone.classList.toggle("nav-unhide");
- navRotate.classList.toggle("nav-rotate");
+ let screenWidth = window.innerWidth;
+
+ if (screenWidth < 768) {
+  dropone.classList.toggle("nav-x");
+  navRotate.classList.toggle("nav-rotate");
+ } else {
+  dropone.classList.remove("nav-x");
+  dropone.classList.toggle("nav-unhide");
+  navRotate.classList.toggle("nav-rotate");
+ }
 });
 document.querySelector(".dropdown-2").addEventListener("click", (event) => {
  event.preventDefault();
- droptwo.classList.toggle("nav-unhide");
- navRotateTwo.classList.toggle("nav-rotate");
+ let screenWidth = window.innerWidth;
+ if (screenWidth >= 768) {
+  droptwo.classList.toggle("nav-unhide");
+  navRotateTwo.classList.toggle("nav-rotate");
+  droptwo.classList.remove("nav-x");
+ } else if (screenWidth < 768) {
+  droptwo.classList.toggle("nav-x");
+  navRotateTwo.classList.toggle("nav-rotate");
+ } else {
+  droptwo.classList.remove("nav-x");
+  alert("why are you here?");
+ }
+});
+
+navAnchor.forEach((button, index) => {
+ button.addEventListener("click", (event) => {
+  event.preventDefault();
+  navAnchor.forEach((otherButton) => {
+   if (otherButton !== button) {
+    otherButton.classList.remove("nav-active");
+   }
+  });
+  button.classList.toggle("nav-active");
+ });
 });
